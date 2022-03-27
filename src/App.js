f81a94a22bbb5495c046da417125e8c0b165c32d
Diego Urbina <70447898+yhonex@users.noreply.github.com>
2022-03-27 04:27:58 -0300

    solucion errores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./assets/css/App.css";
 
 import Routes from "./Routes/Routes";
-import Productstate from "./context/Products/Productstate";
+
 
 function App() {
   return (
     <div className="App">
-      <Productstate>
+     
         <Routes />
-      </Productstate>
+     
     </div>
   );
 }
